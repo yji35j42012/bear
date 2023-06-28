08d82583cd4f8eef05f4226e85a768122127c8c1
yji35j4212 <yji35j4212@gmail.com>
2023-06-28 17:11:21 +0800

    檔案同步

--- components/Card.vue
+++ components/Card.vue
@@ -2,33 +2,15 @@
 
 <template>
 	<ul class="card">
-		<li class="card_item shadow">
+		<li class="card_item shadow" v-for="(item , index) in card_obj" :key="index">
 			<div class="card_item_pic">
 				<img src="./images/map/map1.png" alt />
 			</div>
 			<div class="card_item_txt">
-				<h3 class="title">全國素食-大安復興店</h3>
-				<h4 class="address msgL" data-msg-l="地址：">台北市大 安區復興南路二段314號</h4>
-				<h4 class="time msgL" data-msg-l="營業時間：">每日早上10：30～13：50，下午16：30～20：00</h4>
-				<h4 class="phone msgL" data-msg-l="電話：">02-27361168</h4>
-				<ul class="sort">
-					<li class="sort_item">全素</li>
-					<li class="sort_item">蛋素</li>
-					<li class="sort_item">奶素</li>
-				</ul>
-				<button class="normal_btn">查看地圖</button>
-				<i class="icon_collect" v-html="icon_all.collect"></i>
-			</div>
-		</li>
-		<li class="card_item shadow">
-			<div class="card_item_pic">
-				<img src="./images/map/map1.png" alt />
-			</div>
-			<div class="card_item_txt">
-				<h3 class="title">全國素食-大安復興店</h3>
-				<h4 class="address msgL" data-msg-l="地址：">台北市大 安區復興南路二段314號</h4>
-				<h4 class="time msgL" data-msg-l="營業時間：">每日早上10：30～13：50，下午16：30～20：00</h4>
-				<h4 class="phone msgL" data-msg-l="電話：">02-27361168</h4>
+				<h3 class="title">{{item.title}}</h3>
+				<h4 class="address msgL" data-msg-l="地址：">{{item.address}}</h4>
+				<h4 class="time msgL" data-msg-l="營業時間：">{{item.time}}</h4>
+				<h4 class="phone msgL" data-msg-l="電話：">{{item.phone}}</h4>
 				<ul class="sort">
 					<li class="sort_item">全素</li>
 					<li class="sort_item">蛋素</li>
@@ -48,6 +30,7 @@ module.exports = {
 			icon_all: icon_all
 		};
 	},
+	props: ["card_obj"],
 	components: {},
 	mounted() {},
 	computed: {},
