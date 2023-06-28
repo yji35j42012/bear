c56a86f3d543ea120767e55420a8193a4ce01798
yji35j4212 <yji35j4212@gmail.com>
2023-06-28 16:42:35 +0800

    檔案同步

--- js/icon_all.js
+++ js/icon_all.js
@@ -1,1 +1,7 @@
-var icon_all = {};
+var icon_all = {
+	collect:`<svg width="25" height="32" viewBox="0 0 25 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g id="collect">
+	<path id="Vector" d="M1.5 30.176V3.89478C1.5 2.39323 2.7312 1.17603 4.25 1.17603H20.75C22.2688 1.17603 23.5 2.39323 23.5 3.89478V30.176L12.5 23.8323L1.5 30.176Z"  stroke="#333333" stroke-width="2"/>
+    </g>
+    </svg>`,
+};
